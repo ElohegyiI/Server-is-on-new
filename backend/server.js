ec47f9ed5269f551d6c0a57c26e3355fecab43a7
ElohegyiI <roxnadz78@gmail.com>
2022-03-29 17:57:45 +0200

    update

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+/*const express = require('express')
+
+const path = require('path')
+
+const app = express()
+
+app.get('/', (request, response) => {
+
+	response.sendFile(path.join(`${__dirname}/../frontend/index.html`))
+
+})
+app.use('/public', express.static(`${__dirname}/../frontend/public`))
+
+app.listen(9000, () => {
+
+console.log("http://127.0.0.1:9000")
+
+})*/
+
+const express = require('express')
+
+//const fetch = require("node-fetch")
+
+const res = require('express/lib/response')
+
+const app = express()
+
+const port = 9000
+
+
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello World! It's Codecool!</h1>
+  <h2>Hello</h2>`)
+});
+
+app.get('/frontend', (req, res) => {
+  res.send(`<h2>Hello</h2>`)
+});
+
+/*app.get('/backend/data', (req, res) => {
+  res.send(`<p>Data</p>`)
+});*/
+
+app.use('/frontend', express.static('frontend'))
+
+
+
+app.listen(port, () => {
+
+  console.log(`http://127.0.0.1:${port}`)
+
+})
---
+++ frontend/api/script.js
@@ -0,0 +1,19 @@
+
+
+
+fetch("./api/students.json")
+
+    .then(response => response.json())
+
+        .then(data => {
+
+            for (const student of data.cards) {
+
+                document.getElementById("root").insertAdjacentHTML('beforeend', `
+                <div class="cards">
+                    <h2>${student.id}</h2>
+                    <p>${student.name}</p>
+                    <p>${student.status}</p>                
+                `)
+            }
+        });
